cca6f68e12c5c5e441bb35bab7027d0c5a19da1a
apl <apl@opencascade.com>
2017-04-28 09:17:05 +0300

    0030791: Visualization - possibility to display materials by different hatching style for clipping
    (cherry picked from commit d8c0506f550ad500ea2e8cde5d9e0a718a6698b7)
    after cherry pick, it was ported on 7.5.0 and improved
    (cherry picked from commit 23f0657aa572b8f775e9035c95491822d01e04da)
    
    # Conflicts:
    #	src/AIS/AIS_Shape.cxx
    #	src/OpenGl/OpenGl_CappingAlgo.cxx
    #	src/OpenGl/OpenGl_CappingPlaneResource.cxx
    #	src/OpenGl/OpenGl_CappingPlaneResource.hxx
    #	src/ViewerTest/ViewerTest_ViewerCommands.cxx
    #	tests/bugs/vis/bug24224
    #	tests/bugs/vis/bug27751_capping

--- src/Prs3d/Prs3d_Drawer.cxx
+++ src/Prs3d/Prs3d_Drawer.cxx
@@ -15,6 +15,7 @@
 #include <Prs3d_Drawer.hxx>
 
 #include <Graphic3d_AspectFillArea3d.hxx>
+#include <Graphic3d_AspectFillCapping.hxx>
 #include <Graphic3d_AspectMarker3d.hxx>
 #include <Graphic3d_AspectText3d.hxx>
 #include <Prs3d_ArrowAspect.hxx>
@@ -105,7 +106,9 @@ Prs3d_Drawer::Prs3d_Drawer()
   myHasOwnDimLengthModelUnits   (Standard_False),
   myHasOwnDimAngleModelUnits    (Standard_False),
   myHasOwnDimLengthDisplayUnits (Standard_False),
-  myHasOwnDimAngleDisplayUnits  (Standard_False)
+  myHasOwnDimAngleDisplayUnits  (Standard_False),
+
+  myHasOwnFillCappingAspect     (Standard_False)
 {
   myDimensionModelUnits.SetLengthUnits ("m");
   myDimensionModelUnits.SetAngleUnits ("rad");
@@ -401,6 +404,27 @@ void Prs3d_Drawer::SetDimensionAspect (const Handle(Prs3d_DimensionAspect)& theA
 {
   myDimensionAspect = theAspect;
   myHasOwnDimensionAspect = !myDimensionAspect.IsNull();
+}
+
+// =======================================================================
+// function : FillCappingAspect
+// purpose  :
+// =======================================================================
+
+const Handle(Graphic3d_AspectFillCapping)& Prs3d_Drawer::FillCappingAspect()
+{
+  return myFillCappingAspect;
+}
+
+// =======================================================================
+// function : SetFillCappingAspect
+// purpose  :
+// =======================================================================
+
+void Prs3d_Drawer::SetFillCappingAspect (const Handle(Graphic3d_AspectFillCapping)& theAspect)
+{
+  myFillCappingAspect = theAspect;
+  myHasOwnFillCappingAspect = !myFillCappingAspect.IsNull();
 }
 
 // =======================================================================
