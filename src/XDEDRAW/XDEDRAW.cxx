379c840789d36414fcb72180d8f164633b139109
nds <nds@opencascade.com>
2018-09-27 17:53:53 +0300

    refs 474:Possibility to create and play animations
    (cherry picked from commit c7b1056827771b8a988f329e528b737584b2cbeb)
    
    # Conflicts:
    #	src/BinMXCAFDoc/BinMXCAFDoc.hxx
    #	src/BinMXCAFDoc/FILES
    #	src/XmlMXCAFDoc/FILES

--- src/XDEDRAW/XDEDRAW.cxx
+++ src/XDEDRAW/XDEDRAW.cxx
@@ -92,6 +92,7 @@
 #include <XDEDRAW_GDTs.hxx>
 #include <XDEDRAW_Views.hxx>
 #include <XDEDRAW_Notes.hxx>
+#include <XDEDRAW_Animations.hxx>
 #include <XSDRAW.hxx>
 #include <XSDRAWIGES.hxx>
 #include <XSDRAWSTEP.hxx>
@@ -1498,6 +1499,7 @@ void XDEDRAW::Init(Draw_Interpretor& di)
   XDEDRAW_GDTs::InitCommands ( di );
   XDEDRAW_Views::InitCommands(di);
   XDEDRAW_Notes::InitCommands(di);
+  XDEDRAW_Animations::InitCommands( di );
   XDEDRAW_Common::InitCommands ( di );//moved from EXE
 
 }
