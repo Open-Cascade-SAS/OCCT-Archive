dd9cb11a2062c3bed8bc9a18f0c3e0da8ac567cd
nds <nds@opencascade.com>
2020-09-08 00:16:32 +0300

    0031326: Foundation Classes - Init from Json for base OCCT classes
    InitFromJson method implementation for some simple classes.
    OCCT_INIT_* defines introduction to do automatic parsing of the stream into values.
    Inspector is extended to visualize objects created on the dump stream if it might be created.
    
    (cherry picked from commit 6b63dc83c3b18514fa808afb1c069cd82f895d44)
    
    # Conflicts:
    #	src/Standard/Standard_Dump.hxx

--- src/Quantity/Quantity_Color.hxx
+++ src/Quantity/Quantity_Color.hxx
@@ -267,6 +267,9 @@ Standard_Boolean operator == (const Quantity_Color& Other) const
   //! Dumps the content of me into the stream
   Standard_EXPORT void DumpJson (Standard_OStream& theOStream, Standard_Integer theDepth = -1) const;
 
+  //! Inits the content of me from the stream
+  Standard_EXPORT Standard_Boolean InitFromJson (const Standard_SStream& theSStream, Standard_Integer& theStreamPos);
+
 private:
 
   //! Converts HLS components into RGB ones.
