Mike-Gao/occt

0030988: Add possibility to attach multiple files to notes
Implemented

src/XCAFDoc/XCAFDoc_NoteBalloon.hxx
@@ -14,15 +14,15 @@
 #ifndef _XCAFDoc_NoteBalloon_HeaderFile
 #define _XCAFDoc_NoteBalloon_HeaderFile
 
-#include <XCAFDoc_NoteComment.hxx>
+#include <XCAFDoc_Note.hxx>
 
 //! A comment note attribute.
 //! Contains a textual comment.
-class XCAFDoc_NoteBalloon : public XCAFDoc_NoteComment
+class XCAFDoc_NoteBalloon : public XCAFDoc_Note
 {
 public:
 
-  DEFINE_STANDARD_RTTIEXT(XCAFDoc_NoteBalloon, XCAFDoc_NoteComment)
+  DEFINE_STANDARD_RTTIEXT(XCAFDoc_NoteBalloon, XCAFDoc_Note)
 
   //! Returns default attribute GUID
   Standard_EXPORT static const Standard_GUID& GetID();
@@ -43,11 +43,21 @@ public:
   //! Creates an empty comment note.
   Standard_EXPORT XCAFDoc_NoteBalloon();
 
+  //! Sets the comment text.
+  Standard_EXPORT void Set(const TCollection_ExtendedString& theComment);
+
+  //! Returns the comment text.
+  const TCollection_ExtendedString& Get() const { return myComment; }
+
 public:
 
   // Overrides TDF_Attribute virtuals
   Standard_EXPORT const Standard_GUID& ID() const Standard_OVERRIDE;
   Standard_EXPORT Handle(TDF_Attribute) NewEmpty() const Standard_OVERRIDE;
+
+private:
+
+  TCollection_ExtendedString myComment;
 
 };
 

Open-Cascade-SAS/OCCT-Archive

- Implemented.
(cherry pickedd from commit 0a63642bc5219ee894ab1225664c3f023a9a572f)
- Swap child/father in RemoveFromGroup method of NotesTool.
(cherry picked from commit 9640291ff0e38a6f1e06f5f77f9dda7bcc109082)

src/XCAFDoc/XCAFDoc_NoteBalloon.hxx
@@ -14,15 +14,15 @@
 #ifndef _XCAFDoc_NoteBalloon_HeaderFile
 #define _XCAFDoc_NoteBalloon_HeaderFile
 
-#include <XCAFDoc_NoteComment.hxx>
+#include <XCAFDoc_Note.hxx>
 
 //! A comment note attribute.
 //! Contains a textual comment.
-class XCAFDoc_NoteBalloon : public XCAFDoc_NoteComment
+class XCAFDoc_NoteBalloon : public XCAFDoc_Note
 {
 public:
 
-  DEFINE_DERIVED_ATTRIBUTE(XCAFDoc_NoteBalloon, XCAFDoc_NoteComment)
+  DEFINE_DERIVED_ATTRIBUTE(XCAFDoc_NoteBalloon, XCAFDoc_Note)
 
   //! Returns default attribute GUID
   Standard_EXPORT static const Standard_GUID& GetID();
@@ -43,10 +43,20 @@ public:
   //! Creates an empty comment note.
   Standard_EXPORT XCAFDoc_NoteBalloon();
 
+  //! Sets the comment text.
+  Standard_EXPORT void Set(const TCollection_ExtendedString& theComment);
+
+  //! Returns the comment text.
+  const TCollection_ExtendedString& Get() const { return myComment; }
+
 public:
 
   // Overrides TDF_Attribute virtuals
   Standard_EXPORT const Standard_GUID& ID() const Standard_OVERRIDE;
+
+private:
+
+  TCollection_ExtendedString myComment;
 };
 
 DEFINE_STANDARD_HANDLE(XCAFDoc_NoteBalloon, XCAFDoc_NoteComment)