Open-Cascade-SAS/OCCT

0032289: Visualization - add NCollection_Mat3 for 3x3 matrix similar to NCollection_Mat4
Introduced NCollection_Mat3 class similar to NCollection_Mat4.
Added NCollection_Mat4::operator() alias to NCollection_Mat4::Value().
Added NCollection_Mat4::Negated(), ::Subtracted(), ::Added(), ::Divided() operations.
Added a note to NCollection_Mat4::GetData() that matrix values are stored in column-major order.
Removed duplicated operator== operators from NCollection_Mat4/NCollection_Vec4/NCollection_Vec3/NCollection_Vec2 classes.

Removed obsolete and no more used matrix state fields from OpenGl_Workspace.
Removed obsoiete structure OpenGl_Matrix (replaced by OpenGl_Mat4).
OpenGl_ShaderProgram - duplicated methods have been replaced by templates.

src/OpenGl/OpenGl_Context.cxx
@@ -2657,7 +2657,7 @@ void OpenGl_Context::SetTextureMatrix (const Handle(Graphic3d_TextureParams)& th
       Graphic3d_TransformUtils::Translate (aTextureMat, -aTrans.x(), -aTrans.y(), 0.0f);
     }
     Graphic3d_TransformUtils::Rotate (aTextureMat, -theParams->Rotation(), 0.0f, 0.0f, 1.0f);
-    core11ffp->glLoadMatrixf (aTextureMat);
+    core11ffp->glLoadMatrixf (aTextureMat.GetData());
     core11ffp->glMatrixMode (aMatrixMode);
   }
 #endif

src/OpenGl/OpenGl_ShaderManager.cxx
@@ -597,7 +597,7 @@ void OpenGl_ShaderManager::pushProjectionState (const Handle(OpenGl_ShaderProgra
     if (myContext->core11ffp != NULL)
     {
       myContext->core11ffp->glMatrixMode (GL_PROJECTION);
-      myContext->core11ffp->glLoadMatrixf (myProjectionState.ProjectionMatrix());
+      myContext->core11ffp->glLoadMatrixf (myProjectionState.ProjectionMatrix().GetData());
     }
   #endif
     return;

Open-Cascade-SAS/OCCT-Archive

src/OpenGl/OpenGl_Context.cxx
@@ -4206,7 +4206,7 @@ void OpenGl_Context::SetTextureMatrix (const Handle(Graphic3d_TextureParams)& th
       Graphic3d_TransformUtils::Translate (aTextureMat, -aTrans.x(), -aTrans.y(), 0.0f);
     }
     Graphic3d_TransformUtils::Rotate (aTextureMat, -theParams->Rotation(), 0.0f, 0.0f, 1.0f);
-    core11->glLoadMatrixf (aTextureMat);
+    core11->glLoadMatrixf (aTextureMat.GetData());
     core11->glMatrixMode (aMatrixMode);
   }
 #endif

src/OpenGl/OpenGl_ShaderManager.cxx
@@ -978,7 +978,7 @@ void OpenGl_ShaderManager::pushProjectionState (const Handle(OpenGl_ShaderProgra
     if (myContext->core11 != NULL)
     {
       myContext->core11->glMatrixMode (GL_PROJECTION);
-      myContext->core11->glLoadMatrixf (myProjectionState.ProjectionMatrix());
+      myContext->core11->glLoadMatrixf (myProjectionState.ProjectionMatrix().GetData());
     }
   #endif
     return;