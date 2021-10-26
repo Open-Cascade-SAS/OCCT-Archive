97ec42b02ad38dd87421defa5174eeeb261f9e8a
dpasukhi <dpasukhi@opencascade.com>
2021-10-26 23:45:42 +0300

    0028104: Data Exchange - Extract sub-assembly (XDE)
    - Added getting and setting Material, VisMaterial and Props to XCAFDoc_Editor::Expand.
    - Added XCAFDoc_Editor::Extract to transfer part to a new location
      - XCAFDoc_Editor::Extract allows to extract a part as a component or simply extract to an empty label with attached colors, layers, materials, vismaterials and props
    - Added XCAFDoc_Editor::CloneMetaData to clone shape's attributes to new location
    - Added XCAFDoc_Editor::CloneShapeLabel to clone shape to new document
    - Added XCAFDoc_DocumentTool::Check*Tool methods to check an existing of an any tool
    - Protected against creating a new label for non-existent tools via extract or expand process

--- src/XCAFDoc/XCAFDoc_Editor.hxx
+++ src/XCAFDoc/XCAFDoc_Editor.hxx
@@ -16,59 +16,64 @@
 #ifndef _XCAFDoc_Editor_HeaderFile
 #define _XCAFDoc_Editor_HeaderFile
 
-#include <Standard.hxx>
 #include <Standard_DefineAlloc.hxx>
-#include <Standard_Macro.hxx>
-
-#include <Standard_Boolean.hxx>
 #include <TDataStd_Name.hxx>
+#include <TDF_AttributeMap.hxx>
 #include <TDF_Label.hxx>
+#include <TDF_LabelDataMap.hxx>
 #include <TDF_LabelSequence.hxx>
 
-
+class XCAFDoc_VisMaterial;
+class XCAFDoc_ShapeTool;
 
 //! Tool for edit structure of document.
-class XCAFDoc_Editor 
+class XCAFDoc_Editor
 {
 public:
 
   DEFINE_STANDARD_ALLOC
 
-  
   //! Convert Shape (compound/compsolid/shell/wire) to assembly.
   //! Only compounds expanded recursively
-  Standard_EXPORT static   Standard_Boolean Expand (const TDF_Label& Doc, const TDF_Label& Shape, const Standard_Boolean recursively = Standard_True) ;
-  
+  Standard_EXPORT static Standard_Boolean Expand(const TDF_Label& theDoc,
+                                                 const TDF_Label& theShape,
+                                                 const Standard_Boolean theRecursively = Standard_True);
+
   //! Convert all compounds in Doc to assembly
-  Standard_EXPORT static   Standard_Boolean Expand (const TDF_Label& Doc, const Standard_Boolean recursively = Standard_True) ;
+  Standard_EXPORT static Standard_Boolean Expand(const TDF_Label& theDoc,
+                                                 const Standard_Boolean theRecursively = Standard_True);
 
+  //! Extracts passed labels and put them as a component of theDstLabel
+  //! or creates new shape labels if theDstLabel is a main label of document
+  Standard_EXPORT static Standard_Boolean Extract(const TDF_LabelSequence& theSrcLabels,
+                                                  const TDF_Label& theDstLabel,
+                                                  const Standard_Boolean theIsNoVisMat = Standard_False);
 
+  //! Extracts passed label and put it as a component of theDstLabel
+  //! or creates new shape label if theDstLabel is a main label of document
+  Standard_EXPORT static Standard_Boolean Extract(const TDF_Label& theSrcLabel,
+                                                  const TDF_Label& theDstLabel,
+                                                  const Standard_Boolean theIsNoVisMat = Standard_False);
 
+  //! Copies shapes label with saving of shape structure (recursively)
+  //! theMap is a relating map of the original shapes label and labels created from them
+  Standard_EXPORT static TDF_Label CloneShapeLabel(const TDF_Label& theSrcLabel,
+                                                   const Handle(XCAFDoc_ShapeTool)& theSrcShapeTool,
+                                                   const Handle(XCAFDoc_ShapeTool)& theDstShapeTool,
+                                                   TDF_LabelDataMap& theMap);
 
-protected:
-
-
-
-
-
-private:
-
-  
-  //! Get colors, layers and name from Label
-  Standard_EXPORT static   Standard_Boolean getParams (const TDF_Label& Doc, const TDF_Label& Label, TDF_LabelSequence& Colors, TDF_LabelSequence& Layers, Handle(TDataStd_Name)& Name) ;
-  
-  //! Set colors, layers and name from Label
-  Standard_EXPORT static   Standard_Boolean setParams (const TDF_Label& Doc, const TDF_Label& Label, const TDF_LabelSequence& Colors, const TDF_LabelSequence& Layers, const Handle(TDataStd_Name)& Name) ;
-
-
-
+  //! Copies meta data conteins from the source label to the destination label
+  //! theVisMatMap can be NULL, if theSrcLabel and theDstLabel located in the same document
+  //! Protected against creating a new label for non-existent tools
+  Standard_EXPORT static void CloneMetaData(const TDF_Label& theSrcLabel,
+                                            const TDF_Label& theDstLabel,
+                                            NCollection_DataMap<Handle(XCAFDoc_VisMaterial), Handle(XCAFDoc_VisMaterial)>* theVisMatMap,
+                                            const Standard_Boolean theToCopyColor = Standard_True,
+                                            const Standard_Boolean theToCopyLayer = Standard_True,
+                                            const Standard_Boolean theToCopyMaterial = Standard_True,
+                                            const Standard_Boolean theToCopyVisMaterial = Standard_True,
+                                            const Standard_Boolean theToCopyAttributes = Standard_True);
 
 };
-
-
-
-
-
-
 
 #endif // _XCAFDoc_Editor_HeaderFile
