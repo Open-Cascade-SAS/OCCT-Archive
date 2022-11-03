234ee68005362701ef91aa318445eee6441fb770
inv <igor.nazarov@opencascade.com>
2022-11-03 22:26:10 +0300

    Update

--- get.sh
+++ get.sh
@@ -3,15 +3,17 @@
 problem_file=Adaptor2d_Curve2d.cxx
 
 list="master integration"
-echo "Getting branches hash"
+echo "Getting commit hash"
 for name in $list ; do
     echo $name
     export shorthash_$name=$(git rev-parse $name)
     echo shorthash_$name=$(eval echo "\$shorthash_${name}")
 done
 
+echo "Getting list of commits between master and integration"
 list=$(git rev-list --ancestry-path ${shorthash_master}..${shorthash_integration})
 
+echo "Getting commit whose hash matches the hash of commit with problematic file"
 for hash in $list ; do    
     list_changed_files=$(git diff-tree --no-commit-id --name-only -r $hash | sed 's/\// /g' | awk '{ print $NF }')
     for changed_file in $list_changed_files; do
