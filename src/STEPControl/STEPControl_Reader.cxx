Open-Cascade-SAS/testrepo

Data Exchange - Update Readers with ShapeHealing parameters #247
All instances of using XSAlgo_AlgoContainer are replaced with
XSAlgo_ShapeProcessor.

Parameters for XSAlgo_ShapeProcessor operations are now can be passes
via the updated interface of respective classes.

Staqtic function to read processing parameters from file is added to
XSAlgo_ShapeProcessor.

src/STEPControl/STEPControl_Reader.cxx
@@ -11,7 +11,7 @@
 // Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
-
+#include <DE_ShapeFixParameters.hxx>
 #include <IFSelect_WorkLibrary.hxx>
 #include <Interface_EntityIterator.hxx>
 #include <Interface_Graph.hxx>
@@ -60,6 +60,7 @@
 #include <TColStd_MapOfAsciiString.hxx>
 #include <TColStd_SequenceOfAsciiString.hxx>
 #include <Transfer_TransientProcess.hxx>
+#include <XSAlgo_ShapeProcessor.hxx>
 #include <XSControl_TransferReader.hxx>
 #include <XSControl_WorkSession.hxx>
 
@@ -83,7 +84,7 @@ STEPControl_Reader::STEPControl_Reader
 {
   STEPControl_Controller::Init();
   SetWS (WS,scratch);
-  SetNorm ("STEP");
+  SetNorm("STEP");
 }
 
 //=======================================================================
@@ -650,6 +651,23 @@ inline static TCollection_AsciiString getSiName(const Handle(StepBasic_SiUnit)& 
   };
   return aName;
 }
+
+//=================================================================================================
+
+DE_ShapeFixParameters STEPControl_Reader::GetDefaultParameters() const
+{
+  return DE_ShapeFixParameters::GetDefaultReadingParamsSTEP();
+}
+
+//=================================================================================================
+
+ShapeProcess::OperationsFlags STEPControl_Reader::GetDefaultShapeProcessFlags() const
+{
+  ShapeProcess::OperationsFlags aFlags;
+  aFlags.set(ShapeProcess::Operation::FixShape);
+  return aFlags;
+}
+
 
 //=======================================================================
 //function : findUnits

src/STEPControl/STEPControl_Writer.cxx
@@ -13,6 +13,7 @@
 
 #include <STEPControl_Writer.hxx>
 
+#include <DE_ShapeFixParameters.hxx>
 #include <Interface_InterfaceModel.hxx>
 #include <Interface_Macros.hxx>
 #include <STEPControl_ActorWrite.hxx>
@@ -49,7 +50,7 @@ STEPControl_Writer::STEPControl_Writer
   (const Handle(XSControl_WorkSession)& WS, const Standard_Boolean scratch)
 {
   STEPControl_Controller::Init();
-  SetWS (WS,scratch);
+  SetWS(WS, scratch);
 }
 
 
@@ -167,6 +168,7 @@ IFSelect_ReturnStatus STEPControl_Writer::Transfer
   Handle(STEPControl_ActorWrite) ActWrite =
     Handle(STEPControl_ActorWrite)::DownCast(WS()->NormAdaptor()->ActorWrite());
   ActWrite->SetGroupMode(Handle(StepData_StepModel)::DownCast(thesession->Model())->InternalParameters.WriteAssembly);
+  InitializeMissingParameters();
   return thesession->TransferWriteShape(sh, compgraph, theProgress);
 }
 
@@ -214,4 +216,98 @@ void STEPControl_Writer::PrintStatsTransfer
   (const Standard_Integer what, const Standard_Integer mode) const
 {
   thesession->TransferWriter()->PrintStats (what,mode);
+}
+//=============================================================================
+
+void STEPControl_Writer::SetParameters(const ParameterMap& theParameters)
+{
+  if (Handle(Transfer_ActorOfFinderProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(theParameters);
+  }
+}
+
+//=============================================================================
+
+void STEPControl_Writer::SetParameters(ParameterMap&& theParameters)
+{
+  if (Handle(Transfer_ActorOfFinderProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(std::move(theParameters));
+  }
+}
+
+//=============================================================================
+
+void STEPControl_Writer::SetParameters(const DE_ShapeFixParameters& theParameters,
+                                       const ParameterMap& theAdditionalParameters)
+{
+  if (Handle(Transfer_ActorOfFinderProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(theParameters, theAdditionalParameters);
+  }
+}
+
+//=============================================================================
+
+const STEPControl_Writer::ParameterMap& STEPControl_Writer::GetParameters() const
+{
+  static const ParameterMap                   anEmptyMap;
+  const Handle(Transfer_ActorOfFinderProcess) anActor = GetActor();
+  return anActor.IsNull() ? anEmptyMap : anActor->GetParameters();
+}
+
+//=============================================================================
+
+void STEPControl_Writer::SetShapeProcessFlags(const ShapeProcess::OperationsFlags& theFlags)
+{
+  if (Handle(Transfer_ActorOfFinderProcess) anActor = GetActor())
+  {
+    anActor->SetShapeProcessFlags(theFlags);
+  }
+}
+
+//=============================================================================
+
+const STEPControl_Writer::ProcessingFlags& STEPControl_Writer::GetShapeProcessFlags() const
+{
+  static const ProcessingFlags                anEmptyFlags;
+  const Handle(Transfer_ActorOfFinderProcess) anActor = GetActor();
+  return anActor.IsNull() ? anEmptyFlags : anActor->GetShapeProcessFlags();
+}
+
+//=============================================================================
+
+Handle(Transfer_ActorOfFinderProcess) STEPControl_Writer::GetActor() const
+{
+  Handle(XSControl_WorkSession) aSession = WS();
+  if (aSession.IsNull())
+  {
+    return nullptr;
+  }
+
+  Handle(XSControl_Controller) aController = aSession->NormAdaptor();
+  if (aController.IsNull())
+  {
+    return nullptr;
+  }
+
+  return aController->ActorWrite();
+}
+
+//=============================================================================
+
+void STEPControl_Writer::InitializeMissingParameters()
+{
+  if (GetParameters().empty())
+  {
+    SetParameters(DE_ShapeFixParameters::GetDefaultWritingParamsSTEP());
+  }
+  if (GetShapeProcessFlags().second == false)
+  {
+    ShapeProcess::OperationsFlags aFlags;
+    aFlags.set(ShapeProcess::Operation::SplitCommonVertex);
+    aFlags.set(ShapeProcess::Operation::DirectFaces);
+    SetShapeProcessFlags(aFlags);
+  }
 }

src/XSControl/XSControl_Reader.cxx
@@ -26,6 +26,7 @@
 #include <TopoDS_Shape.hxx>
 #include <Transfer_IteratorOfProcessForTransient.hxx>
 #include <Transfer_TransientProcess.hxx>
+#include <Transfer_ActorOfTransientProcess.hxx>
 #include <XSControl_Controller.hxx>
 #include <XSControl_Reader.hxx>
 #include <XSControl_TransferReader.hxx>
@@ -255,6 +256,7 @@ Standard_Boolean  XSControl_Reader::TransferEntity
   if (start.IsNull()) return Standard_False;
   const Handle(XSControl_TransferReader) &TR = thesession->TransferReader();
   TR->BeginTransfer();
+  InitializeMissingParameters();
   if (TR->TransferOne (start, Standard_True, theProgress) == 0) return Standard_False;
   TopoDS_Shape sh = TR->ShapeResult(start);
   //ShapeExtend_Explorer STU;
@@ -279,6 +281,7 @@ Standard_Integer  XSControl_Reader::TransferList
   Standard_Integer i, nb = list->Length();
   const Handle(XSControl_TransferReader) &TR = thesession->TransferReader();
   TR->BeginTransfer();
+  InitializeMissingParameters();
   ClearShapes();
   ShapeExtend_Explorer STU;
   Message_ProgressScope PS(theProgress, NULL, nb);
@@ -307,6 +310,7 @@ Standard_Integer  XSControl_Reader::TransferRoots (const Message_ProgressRange& 
   const Handle(XSControl_TransferReader) &TR = thesession->TransferReader();
    
   TR->BeginTransfer();
+  InitializeMissingParameters();
   ClearShapes();
   ShapeExtend_Explorer STU;
   Message_ProgressScope PS (theProgress, "Root", nb);
@@ -483,4 +487,92 @@ void XSControl_Reader::GetStatsTransfer (const Handle(TColStd_HSequenceOfTransie
 	}
   }
 }
-						      
+
+//=============================================================================
+
+void XSControl_Reader::SetParameters(const ParameterMap& theParameters)
+{
+  if (Handle(Transfer_ActorOfTransientProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(theParameters);
+  }
+}
+
+//=============================================================================
+
+void XSControl_Reader::SetParameters(ParameterMap&& theParameters)
+{
+  if (Handle(Transfer_ActorOfTransientProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(std::move(theParameters));
+  }
+}
+
+//=============================================================================
+
+void XSControl_Reader::SetParameters(const DE_ShapeFixParameters& theParameters, const ParameterMap& theAdditionalParameters)
+{
+  if (Handle(Transfer_ActorOfTransientProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(theParameters, theAdditionalParameters);
+  }
+}
+
+//=============================================================================
+
+const XSControl_Reader::ParameterMap& XSControl_Reader::GetParameters() const
+{
+  static const ParameterMap anEmptyMap;
+  const Handle(Transfer_ActorOfTransientProcess) anActor = GetActor();
+  return anActor.IsNull() ? anEmptyMap : anActor->GetParameters();
+}
+
+//=============================================================================
+
+void XSControl_Reader::SetShapeProcessFlags(const ShapeProcess::OperationsFlags& theFlags)
+{
+  if (Handle(Transfer_ActorOfTransientProcess) anActor = GetActor())
+  {
+    anActor->SetProcessingFlags(theFlags);
+  }
+}
+
+//=============================================================================
+
+const XSControl_Reader::ProcessingFlags& XSControl_Reader::GetShapeProcessFlags() const
+{
+  static const ProcessingFlags                   anEmptyFlags;
+  const Handle(Transfer_ActorOfTransientProcess) anActor = GetActor();
+  return anActor.IsNull() ? anEmptyFlags : anActor->GetProcessingFlags();
+}
+
+//=============================================================================
+
+Handle(Transfer_ActorOfTransientProcess) XSControl_Reader::GetActor() const
+{
+  Handle(XSControl_WorkSession) aSession = WS();
+  if (aSession.IsNull())
+  {
+    return nullptr;
+  }
+  Handle(XSControl_TransferReader) aTransferReader = aSession->TransferReader();
+  if (aTransferReader.IsNull())
+  {
+    return nullptr;
+  }
+  return aTransferReader->Actor();
+}
+
+//=============================================================================
+
+void XSControl_Reader::InitializeMissingParameters()
+{
+  if (GetParameters().empty())
+  {
+    SetParameters(GetDefaultParameters());
+  }
+  if (!GetShapeProcessFlags().second)
+  {
+    SetShapeProcessFlags(GetDefaultShapeProcessFlags());
+  }
+}

Open-Cascade-SAS/OCCT-Archive

src/STEPControl/STEPControl_Reader.cxx
@@ -11,7 +11,7 @@
 // Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
-
+#include <DE_ShapeFixParameters.hxx>
 #include <IFSelect_WorkLibrary.hxx>
 #include <Interface_EntityIterator.hxx>
 #include <Interface_Graph.hxx>
@@ -60,6 +60,7 @@
 #include <TColStd_MapOfAsciiString.hxx>
 #include <TColStd_SequenceOfAsciiString.hxx>
 #include <Transfer_TransientProcess.hxx>
+#include <XSAlgo_ShapeProcessor.hxx>
 #include <XSControl_TransferReader.hxx>
 #include <XSControl_WorkSession.hxx>
 
@@ -83,7 +84,7 @@ STEPControl_Reader::STEPControl_Reader
 {
   STEPControl_Controller::Init();
   SetWS (WS,scratch);
-  SetNorm ("STEP");
+  SetNorm("STEP");
 }
 
 //=======================================================================
@@ -650,6 +651,23 @@ inline static TCollection_AsciiString getSiName(const Handle(StepBasic_SiUnit)& 
   };
   return aName;
 }
+
+//=================================================================================================
+
+DE_ShapeFixParameters STEPControl_Reader::GetDefaultParameters() const
+{
+  return DESTEP_Parameters::GetDefaultReadingParamsSTEP();
+}
+
+//=================================================================================================
+
+ShapeProcess::OperationsFlags STEPControl_Reader::GetDefaultShapeProcessFlags() const
+{
+  ShapeProcess::OperationsFlags aFlags;
+  aFlags.set(ShapeProcess::Operation::FixShape);
+  return aFlags;
+}
+
 
 //=======================================================================
 //function : findUnits

src/STEPControl/STEPControl_Writer.cxx
@@ -13,6 +13,7 @@
 
 #include <STEPControl_Writer.hxx>
 
+#include <DE_ShapeFixParameters.hxx>
 #include <Interface_InterfaceModel.hxx>
 #include <Interface_Macros.hxx>
 #include <STEPControl_ActorWrite.hxx>
@@ -24,7 +25,7 @@
 #include <TopExp_Explorer.hxx>
 #include <TopoDS_Shape.hxx>
 #include <XSAlgo.hxx>
-#include <XSAlgo_AlgoContainer.hxx>
+#include <XSAlgo_ShapeProcessor.hxx>
 #include <XSControl_WorkSession.hxx>
 #include <UnitsMethods.hxx>
 
@@ -49,7 +50,7 @@ STEPControl_Writer::STEPControl_Writer
   (const Handle(XSControl_WorkSession)& WS, const Standard_Boolean scratch)
 {
   STEPControl_Controller::Init();
-  SetWS (WS,scratch);
+  SetWS(WS, scratch);
 }
 
 
@@ -157,7 +158,7 @@ IFSelect_ReturnStatus STEPControl_Writer::Transfer
   thesession->TransferWriter()->SetTransferMode (mws);
   if (!Model()->IsInitializedUnit())
   {
-    XSAlgo::AlgoContainer()->PrepareForTransfer(); // update unit info
+    XSAlgo_ShapeProcessor::PrepareForTransfer(); // update unit info
     Model()->SetLocalLengthUnit(UnitsMethods::GetCasCadeLengthUnit());
   }
   if (!thesession->Model().IsNull())
@@ -167,6 +168,7 @@ IFSelect_ReturnStatus STEPControl_Writer::Transfer
   Handle(STEPControl_ActorWrite) ActWrite =
     Handle(STEPControl_ActorWrite)::DownCast(WS()->NormAdaptor()->ActorWrite());
   ActWrite->SetGroupMode(Handle(StepData_StepModel)::DownCast(thesession->Model())->InternalParameters.WriteAssembly);
+  InitializeMissingParameters();
   return thesession->TransferWriteShape(sh, compgraph, theProgress);
 }
 
@@ -214,4 +216,98 @@ void STEPControl_Writer::PrintStatsTransfer
   (const Standard_Integer what, const Standard_Integer mode) const
 {
   thesession->TransferWriter()->PrintStats (what,mode);
+}
+//=============================================================================
+
+void STEPControl_Writer::SetParameters(const ParameterMap& theParameters)
+{
+  if (Handle(Transfer_ActorOfFinderProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(theParameters);
+  }
+}
+
+//=============================================================================
+
+void STEPControl_Writer::SetParameters(ParameterMap&& theParameters)
+{
+  if (Handle(Transfer_ActorOfFinderProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(std::move(theParameters));
+  }
+}
+
+//=============================================================================
+
+void STEPControl_Writer::SetParameters(const DE_ShapeFixParameters& theParameters,
+                                       const ParameterMap& theAdditionalParameters)
+{
+  if (Handle(Transfer_ActorOfFinderProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(theParameters, theAdditionalParameters);
+  }
+}
+
+//=============================================================================
+
+const STEPControl_Writer::ParameterMap& STEPControl_Writer::GetParameters() const
+{
+  static const ParameterMap                   anEmptyMap;
+  const Handle(Transfer_ActorOfFinderProcess) anActor = GetActor();
+  return anActor.IsNull() ? anEmptyMap : anActor->GetParameters();
+}
+
+//=============================================================================
+
+void STEPControl_Writer::SetShapeProcessFlags(const ShapeProcess::OperationsFlags& theFlags)
+{
+  if (Handle(Transfer_ActorOfFinderProcess) anActor = GetActor())
+  {
+    anActor->SetShapeProcessFlags(theFlags);
+  }
+}
+
+//=============================================================================
+
+const STEPControl_Writer::ProcessingFlags& STEPControl_Writer::GetShapeProcessFlags() const
+{
+  static const ProcessingFlags                anEmptyFlags;
+  const Handle(Transfer_ActorOfFinderProcess) anActor = GetActor();
+  return anActor.IsNull() ? anEmptyFlags : anActor->GetShapeProcessFlags();
+}
+
+//=============================================================================
+
+Handle(Transfer_ActorOfFinderProcess) STEPControl_Writer::GetActor() const
+{
+  Handle(XSControl_WorkSession) aSession = WS();
+  if (aSession.IsNull())
+  {
+    return nullptr;
+  }
+
+  Handle(XSControl_Controller) aController = aSession->NormAdaptor();
+  if (aController.IsNull())
+  {
+    return nullptr;
+  }
+
+  return aController->ActorWrite();
+}
+
+//=============================================================================
+
+void STEPControl_Writer::InitializeMissingParameters()
+{
+  if (GetParameters().empty())
+  {
+    SetParameters(DESTEP_Parameters::GetDefaultWritingParamsSTEP());
+  }
+  if (GetShapeProcessFlags().second == false)
+  {
+    ShapeProcess::OperationsFlags aFlags;
+    aFlags.set(ShapeProcess::Operation::SplitCommonVertex);
+    aFlags.set(ShapeProcess::Operation::DirectFaces);
+    SetShapeProcessFlags(aFlags);
+  }
 }

src/XSControl/XSControl_Reader.cxx
@@ -26,6 +26,7 @@
 #include <TopoDS_Shape.hxx>
 #include <Transfer_IteratorOfProcessForTransient.hxx>
 #include <Transfer_TransientProcess.hxx>
+#include <Transfer_ActorOfTransientProcess.hxx>
 #include <XSControl_Controller.hxx>
 #include <XSControl_Reader.hxx>
 #include <XSControl_TransferReader.hxx>
@@ -255,6 +256,7 @@ Standard_Boolean  XSControl_Reader::TransferEntity
   if (start.IsNull()) return Standard_False;
   const Handle(XSControl_TransferReader) &TR = thesession->TransferReader();
   TR->BeginTransfer();
+  InitializeMissingParameters();
   if (TR->TransferOne (start, Standard_True, theProgress) == 0) return Standard_False;
   TopoDS_Shape sh = TR->ShapeResult(start);
   //ShapeExtend_Explorer STU;
@@ -279,6 +281,7 @@ Standard_Integer  XSControl_Reader::TransferList
   Standard_Integer i, nb = list->Length();
   const Handle(XSControl_TransferReader) &TR = thesession->TransferReader();
   TR->BeginTransfer();
+  InitializeMissingParameters();
   ClearShapes();
   ShapeExtend_Explorer STU;
   Message_ProgressScope PS(theProgress, NULL, nb);
@@ -307,6 +310,7 @@ Standard_Integer  XSControl_Reader::TransferRoots (const Message_ProgressRange& 
   const Handle(XSControl_TransferReader) &TR = thesession->TransferReader();
    
   TR->BeginTransfer();
+  InitializeMissingParameters();
   ClearShapes();
   ShapeExtend_Explorer STU;
   Message_ProgressScope PS (theProgress, "Root", nb);
@@ -483,4 +487,94 @@ void XSControl_Reader::GetStatsTransfer (const Handle(TColStd_HSequenceOfTransie
 	}
   }
 }
-						      
+
+//=============================================================================
+
+void XSControl_Reader::SetParameters(const ParameterMap& theParameters)
+{
+  if (Handle(Transfer_ActorOfTransientProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(theParameters);
+  }
+}
+
+//=============================================================================
+
+void XSControl_Reader::SetParameters(ParameterMap&& theParameters)
+{
+  if (Handle(Transfer_ActorOfTransientProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(std::move(theParameters));
+  }
+}
+
+//=============================================================================
+
+void XSControl_Reader::SetParameters(const DE_ShapeFixParameters& theParameters, const ParameterMap& theAdditionalParameters)
+{
+  if (Handle(Transfer_ActorOfTransientProcess) anActor = GetActor())
+  {
+    anActor->SetParameters(theParameters, theAdditionalParameters);
+  }
+}
+
+//=============================================================================
+
+const XSControl_Reader::ParameterMap& XSControl_Reader::GetParameters() const
+{
+  static const ParameterMap anEmptyMap;
+  const Handle(Transfer_ActorOfTransientProcess) anActor = GetActor();
+  return anActor.IsNull() ? anEmptyMap : anActor->GetParameters();
+}
+
+//=============================================================================
+
+void XSControl_Reader::SetShapeProcessFlags(const ShapeProcess::OperationsFlags& theFlags)
+{
+  if (Handle(Transfer_ActorOfTransientProcess) anActor = GetActor())
+  {
+    anActor->SetProcessingFlags(theFlags);
+  }
+}
+
+//=============================================================================
+
+const XSControl_Reader::ProcessingFlags& XSControl_Reader::GetShapeProcessFlags() const
+{
+  static const ProcessingFlags                   anEmptyFlags;
+  const Handle(Transfer_ActorOfTransientProcess) anActor = GetActor();
+  return anActor.IsNull() ? anEmptyFlags : anActor->GetProcessingFlags();
+}
+
+//=============================================================================
+
+Handle(Transfer_ActorOfTransientProcess) XSControl_Reader::GetActor() const
+{
+  Handle(XSControl_WorkSession) aSession = WS();
+  if (aSession.IsNull())
+  {
+    return nullptr;
+  }
+
+  Handle(XSControl_TransferReader) aReader = aSession->TransferReader();
+  if (aReader.IsNull())
+  {
+    return nullptr;
+  }
+
+  return aReader->Actor();
+}
+
+//=============================================================================
+
+void XSControl_Reader::InitializeMissingParameters()
+{
+  if (GetParameters().empty())
+  {
+    SetParameters(GetDefaultParameters());
+  }
+  if (!GetShapeProcessFlags().second)
+  {
+    SetShapeProcessFlags(GetDefaultShapeProcessFlags());
+  }
+}